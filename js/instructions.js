f2305539ee124c65fbf745df9a8da89427496582
Fiona <fihe002@stud.ek.dk>
2025-11-03 21:02:32 +0100

    hover effekt virker

--- js/instructions.js
+++ js/instructions.js
@@ -11,17 +11,39 @@ function proviant() {
 document.addEventListener("DOMContentLoaded", function () {
   const knap = document.getElementById("proviant");
   const kokosnoed = document.getElementById("kokosnoed");
+  const overskrift_faktaboks = document.getElementById("overskrift_faktaboks");
 
   knap.addEventListener("click", function () {
     kokosnoed.classList.toggle("stor");
+
+    if (kokosnoed.classList.contains("stor")) {
+      overskrift_faktaboks.textContent = "FIND MAD";
+    } else {
+      overskrift_faktaboks.textContent = "Proviant";
+    }
   });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const knap = document.getElementById("raab");
-  const sos = document.getElementById("sos");
+  const sos1 = document.getElementById("sos1");
 
   knap.addEventListener("click", function () {
-    sos.classList.toggle("stor");
+    sos1.classList.toggle("stor");
+
+    if (sos1.classList.contains("stor")) {
+      overskrift_faktaboks.textContent = "UNDSLIP";
+    } else {
+      overskrift_faktaboks.textContent = "Et råb om hjælp";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const knap = document.getElementById("lejr");
+  const trae_i_vand = document.getElementById("trae_i_vand");
+
+  knap.addEventListener("click", function () {
+    trae_i_vand.classList.toggle("stor");
   });
 });
