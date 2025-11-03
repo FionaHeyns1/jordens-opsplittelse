3a700a3b6018cc62258206ffa690246c7a81ab23
Fiona <fihe002@stud.ek.dk>
2025-11-03 11:29:48 +0100

    Styling og hover effekt

--- js/instructions.js
+++ js/instructions.js
@@ -7,3 +7,21 @@ function proviant() {
 
   document.querySelector("#kokosnoed").classList.add("my_scale");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const knap = document.getElementById("proviant");
+  const kokosnoed = document.getElementById("kokosnoed");
+
+  knap.addEventListener("click", function () {
+    kokosnoed.classList.toggle("stor");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const knap = document.getElementById("raab");
+  const sos = document.getElementById("sos");
+
+  knap.addEventListener("click", function () {
+    sos.classList.toggle("stor");
+  });
+});
