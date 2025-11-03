fe05c8fbd8d0abff6547effb5fcacd376c65fbe8
Fiona <fihe002@stud.ek.dk>
2025-11-03 09:21:34 +0100

    tilføjer c prop ændringer

--- js/instructions.js
+++ js/instructions.js
@@ -1,9 +1,3 @@
 console.log("loaded instructions");
 
-document.querySelector("#button_1").addEventListener("click", button_1);
-
-function button_1() {
-  console.log("Button is here");
-
-  document.querySelector("#kokosnød").classList.add("my_scale");
-}
+document.querySelector("#proviant").addEventListener("click", proviant);
